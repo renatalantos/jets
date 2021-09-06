362086cd284dd981ffa314316414fdf8742bf5e8
renatalantos <renatalantos@gmail.com>
2021-09-06 17:16:13 +0000

    Breaking test and adding 2 parameters to addition function to make it work again

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,24 @@
+const addition = require("../calc");
+
+describe("Calculator", ()=> {
+    describe("Addition function", ()=> {
+        test("Should return 42 for 20 + 22", ()=>{
+            expect(addition(20,22)).toBe(42);
+        });
+
+        test("Should return 73 for 42 + 31", ()=>{
+            expect(addition(42,31)).toBe(73);
+        });
+
+    });
+    describe("Subtraction function", ()=> {
+
+    });
+    describe("Multiplication function", ()=> {
+
+    });
+    describe("Division function", ()=> {
+
+    });
+
+})
